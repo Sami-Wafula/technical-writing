92e193f900a3fcdcd3360677c4bb1324932faf5c
Sami <samiwafula3257@gmail.com>
2022-07-22 14:45:34 +0300

    add local.c demo for local scope variable declaration

---
+++ variables/local.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - declares local variables a, b and c
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 10, b = 20, c;
+
+	c = a + b;
+	printf("The value of a = %d, b = %d and a + b = %d\n", a, b, c);
+	return (0);
+}
