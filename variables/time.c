294dfb1fbc53a9b63959b48d48cbee8be54d8b9c
Sami <samiwafula3257@gmail.com>
2022-07-29 09:53:23 +0300

    add time.c file for demonstration of static variables, first case scenario

---
+++ variables/time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int times(void);
+int main(void)
+{
+	printf("Number of function executions: %d\n", times());
+	printf("Number of function executions: %d\n", times());
+	return (0);
+}
+int times(void)
+{
+	int time = 0;
+	time++;
+	return (time);
+}
+
+
