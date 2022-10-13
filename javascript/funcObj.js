9ad1f0126d6983dca6450a15d1075b092b983738
Sami-Wafula <samiwafula3257@gmail.com>
2022-10-13 21:21:30 +0300

    function assignment to variable

--- javascript/funcObj.js
+++ javascript/funcObj.js
@@ -1,4 +1,11 @@
 function sum(a, b){
     return a + b;
 }
-console.log(sum(1, 2))
+console.log(sum(1, 2)) //returns 3
+
+// function name assignment to variable
+
+let add = sum
+
+console.log(add(1, 2)) //return 3
+
