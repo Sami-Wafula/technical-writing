f8b8a3b0668fb0cf0c67b4bb36abb82642a21751
Sami-Wafula <samiwafula3257@gmail.com>
2022-10-14 08:39:18 +0300

    add functions

---
+++ javascript/nestedFunction.js
@@ -0,0 +1,15 @@
+//child function
+
+function floatInt (a){
+    let int = parseInt(a);
+    return int;
+}
+
+console.log(floatInt(23.67)) //test - returns 23
+
+function mul(a, b){
+    let res = a * b;
+    return res;
+}
+
+console.log(mul(2.0, 2.9)) //test - returns 5.8
