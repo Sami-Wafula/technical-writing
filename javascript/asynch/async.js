bb697bdb80dc8f6c739f58fa6dd3dfeef82f3137
Sami-Wafula <samiwafula3257@gmail.com>
2022-10-16 17:08:51 +0300

    promise

--- javascript/asynch/async.js
+++ javascript/asynch/async.js
@@ -9,12 +9,30 @@
 
 //asynch code 
 
-const fakeApi = () => {
-    setTimeout(() => {
-        console.log('Sync code');
-    }, 1000);
-}
+// const fakeApi = () => {
+//     setTimeout(() => {
+//         console.log('Sync code');
+//     }, 1000);
+// }
 
-console.log('Hello');
-fakeApi();
-console.log('The end')
+// console.log('Hello');
+// fakeApi();
+// console.log('The end')
+
+//promise
+  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+    if (x == 0) {
+      myResolve("OK");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then(
+    function(value) {console.log(value);},
+    function(error) {console.log(error);}
+  );
+
