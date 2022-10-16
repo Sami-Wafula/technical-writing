6e4963dad11ef5ed02323b251d37dfbb41da1281
Sami-Wafula <samiwafula3257@gmail.com>
2022-10-16 14:32:07 +0300

    async code

--- javascript/asynch/async.js
+++ javascript/asynch/async.js
@@ -1,4 +1,14 @@
 //synchronous execution
+// const fakeApi = () => {
+//     console.log('Sync code');
+// }
+
+// console.log('Hello');
+// fakeApi();
+// console.log('The end')
+
+//asynch code 
+
 const fakeApi = () => {
     console.log('Sync code');
 }
